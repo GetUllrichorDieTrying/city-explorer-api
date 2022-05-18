07c0118ccee0c7d81a5e51f14e242ccf082766c5
DYLAN ULLRICH <dylan@getullrichordietrying.com>
2022-05-17 23:51:00 -0700

    server started

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 'use strict';
 
-console.log('Our first server');
-
 // REQUIRE
 const express = require('express');
 require('dotenv').config();
+let data = require('./data/weather.json');
 
 // USE
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 // ROUTES
-app.get('/', (request, response) => {
-  response.send('Hello from the SERVER down UNDR')
-});
-
 app.get('/weather', (request, response) => {
-  console.log(request.query.name);
-  response.send('HELLO MOTTER TRUCKA')
+  let cityData = request.query.city;
+
+  let searchedCity = data.find(city => city.city_name.toLowerCase() === cityData.toLowerCase());
+
+  let dataToSend = searchedCity.data.
+
 });
 
 // Catch all "star route"***
@@ -29,7 +28,12 @@ app.get('*', (request, response) => {
 
 
 // CLASSES
-
+class Forecast {
+  constructor(weatherObj) {
+    this.date = weatherObj.date;
+    this.description = weatherObj.description;
+  }
+}
 
 // LISTEN
 app.listen(PORT, () => console.log(`LISTENING ON YO PORT ${PORT}`));
